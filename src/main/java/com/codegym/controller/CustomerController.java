745e999fdbdf3761a065695d708276f89bc6d748
ptthao0801 <ptthao0801@gmail.com>
2024-06-10 17:52:55 +0700

    update

---
+++ src/main/java/com/codegym/controller/CustomerController.java
@@ -0,0 +1,40 @@
+package com.codegym.controller;
+
+import com.codegym.model.Customer;
+import com.codegym.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customers")
+public class CustomerController {
+    @Autowired
+    private ICustomerService customerService;
+
+    @GetMapping()
+    public ModelAndView showList() {
+        ModelAndView modelAndView = new ModelAndView("list");
+        List<Customer> customers = customerService.findAll();
+        modelAndView.addObject("customers", customers);
+        return modelAndView;
+    }
+    @GetMapping("/{id}")
+    public ModelAndView showInformation(@PathVariable("id") Long customerId){
+        ModelAndView modelAndView = new ModelAndView("info");
+        Customer customer = customerService.findById(customerId);
+        modelAndView.addObject("customer", customer);
+        return modelAndView;
+    }
+    @PostMapping
+    public String updateCustomer(Customer customer){
+        customerService.save(customer);
+        return "redirect:/customers";
+    }
+}
